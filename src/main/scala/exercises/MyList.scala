3dd4f5a71e6fbbcafb4f175a081da2b9908ed90f
Diego Lopes <diegopereiralopes98@gmail.com>
2023-03-26 17:12:04 -0300

    feat(exercises -> my list): add generics covariant types

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -1,35 +1,36 @@
 package exercises
 
-abstract class MyList {
-  def head: Int
-  def tail: MyList
+abstract class MyList[+A] {
+  def head: A
+  def tail: MyList[A]
   def isEmpty: Boolean
-  def add(number: Int): MyList
+  def add[B >: A](element: B): MyList[B]
   def printElements: String
   override def toString: String = s"[$printElements]"
 }
 
-object Empty extends MyList {
-  def head: Int = throw new NoSuchElementException
-  def tail: MyList = throw new NoSuchElementException
+object Empty extends MyList[Nothing] {
+  def head: Nothing = throw new NoSuchElementException
+  def tail: MyList[Nothing] = throw new NoSuchElementException
   def isEmpty: Boolean = true
-  def add(element: Int): MyList = new Cons(element, Empty)
+  def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
   def printElements: String = ""
 }
 
-class Cons(h: Int, t: MyList) extends MyList {
-  def head: Int = h
-  def tail: MyList = t
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+  def head: A = h
+  def tail: MyList[A] = t
   def isEmpty: Boolean = false
-  def add(element: Int): MyList = new Cons(element, this)
+  def add[B >: A](element: B): MyList[B] = new Cons(element, this)
   def printElements: String =
     if(t.isEmpty) s"$h"
     else s"$h, ${t.printElements}"
 }
 
 object ListTest extends App {
-  private val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
-  println(list.head)
-  println(list.printElements)
-  println(list.toString)
+  private val listOfIntegers = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  private val listOfStrings = new Cons("Hello", new Cons("Scala", new Cons("Learner", Empty)))
+
+  println(listOfIntegers.toString)
+  println(listOfStrings.toString)
 }
