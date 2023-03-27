d5e8b53fc898ff23f3a58ebf4f7c71fc909eb830
Diego Lopes <diegopereiralopes98@gmail.com>
2023-03-27 18:03:30 -0300

    feat(exercises -> my list): add foreach, fold and zipWith

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -13,8 +13,11 @@ abstract class MyList[+A] {
   def map[B](transformer: A => B): MyList[B]
   def flatMap[B](transformer: A => MyList[B]): MyList[B]
   def filter(predicate: A => Boolean ): MyList[A]
+  def forEach(func: A => Unit): Unit
+  def sort(func: (A, A) => Int): MyList[A]
+  def zipWith[B, C](list: MyList[B], func: (A, B) => C): MyList[C]
+  def fold[B](acc: B)(func: (B, A) => B): B
 }
-
 case object Empty extends MyList[Nothing] {
   def head: Nothing = throw new NoSuchElementException
   def tail: MyList[Nothing] = throw new NoSuchElementException
@@ -25,6 +28,12 @@ case object Empty extends MyList[Nothing] {
   def map[B](transformer: Nothing => B): MyList[B] = Empty
   def flatMap[B](transformer: Nothing => MyList[B]): MyList[B] = Empty
   def filter(predicate: Nothing => Boolean): MyList[Nothing] = Empty
+  def forEach(func: Nothing => Unit): Unit = {}
+  def sort(func: (Nothing, Nothing) => Int): MyList[Nothing] = Empty
+  def zipWith[B, C](list: MyList[B], func: (Nothing, B) => C): MyList[C] =
+    if (!list.isEmpty) throw new RuntimeException("Lists are not of same size")
+    else Empty
+  def fold[B](acc: B)(func: (B, Nothing) => B): B = acc
 }
 
 case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
@@ -43,6 +52,25 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     new Cons(transformer(h), t.map(transformer))
   def flatMap[B](transformer: A => MyList[B]): MyList[B] =
     transformer(h) ++ t.flatMap(transformer)
+  def forEach(func: A => Unit): Unit = {
+    func(h)
+    tail.forEach(func)
+  }
+  def sort(func: (A,A) => Int): MyList[A] = {
+    def insert(x: A, sortedList: MyList[A]): MyList[A] =
+      if (sortedList.isEmpty) new Cons(x, Empty)
+      else if (func(x, sortedList.head) <= 0) new Cons(x, sortedList)
+      else new Cons(sortedList.head, insert(x, sortedList.tail))
+
+    val sortedTail = t.sort(func)
+    insert(h, sortedTail)
+  }
+  def zipWith[B, C](list: MyList[B], func: (A, B) => C): MyList[C] =
+    if (list.isEmpty) throw new RuntimeException("Lists are not of same size")
+    else new Cons(func(h, list.head), t.zipWith(list.tail, func))
+
+  def fold[B](acc: B)(func: (B, A) => B): B =
+    t.fold(func(acc, h))(func)
 }
 
 object ListTest extends App {
@@ -57,4 +85,10 @@ object ListTest extends App {
   println(listOfIntegers.map(_ * 2).toString)
 
   println(listOfIntegers.flatMap((e: Int) => Cons(e, Cons(e + 1, Empty))).toString)
+
+  listOfStrings.forEach(println(_))
+
+  println(listOfIntegers.zipWith[String, String](listOfStrings, _ + "-" + _))
+
+  println(listOfIntegers.fold(0)((a: Int, b:Int) => a + b))
 }
