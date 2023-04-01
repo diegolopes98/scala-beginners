995de8065e1f398b806e172ac662d02d931c672c
Diego Lopes <diegopereiralopes98@gmail.com>
2023-04-01 13:08:47 -0300

    feat(lecture 3 fp): options

---
+++ src/main/scala/lectures/part3fp/Options.scala
@@ -0,0 +1,73 @@
+package lectures.part3fp
+
+import scala.util.Random
+
+object Options extends App {
+
+  val myFirstOption: Option[Int] = Some(4)
+  val noOption: Option[Int] = None
+
+  println(myFirstOption)
+  println(noOption)
+
+  // WORK with unsafe APIs
+  def unsafeMethod(): String = null
+
+  //  val result = Some(null) // WRONG
+  val result = Option(unsafeMethod()) // Some or None
+  println(result)
+
+  // chained methods
+  def backupMethod(): String = "A valid result"
+
+  val chainedResult = Option(unsafeMethod()).orElse(Option(backupMethod()))
+
+  // DESIGN unsafe APIs
+  def betterUnsafeMethod(): Option[String] = None
+
+  def betterBackupMethod(): Option[String] = Some("A valid result")
+
+  val betterChainedResult = betterUnsafeMethod() orElse betterBackupMethod()
+
+  // functions on Options
+  println(myFirstOption.isEmpty)
+  println(myFirstOption.get) // USAFE - DO NOT USE THIS
+
+  // map, flatMap, filter
+  println(myFirstOption.map(_ * 2))
+  println(myFirstOption.filter(x => x > 10))
+  println(myFirstOption.flatMap(x => Option(x * 10)))
+
+  // for-comprehensions
+
+  /*
+    Exercise.
+   */
+  val config: Map[String, String] = Map(
+    // fetched from elsewhere
+    "host" -> "176.45.36.1",
+    "port" -> "80"
+  )
+
+  class Connection {
+    def connect = "Connected" // connect to some server
+  }
+
+  object Connection {
+    val random = new Random(System.nanoTime())
+
+    def apply(host: String, port: String): Option[Connection] =
+      if (random.nextBoolean()) Some(new Connection)
+      else None
+  }
+  // try to establish a connection, if so - print the connect method
+
+  val connection: Option[String] = for {
+    host <- config.get("host")
+    port <- config.get("port")
+    c   <- Connection(host, port)
+  } yield c.connect
+
+  if (connection.isDefined) println(connection)
+  else println("error connecting to the server")
+}
